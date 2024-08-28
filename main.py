53661e9df646b784012afec57906b0a485ccb9b6
Victor Sultanum <145617965+21sult@users.noreply.github.com>
2024-08-28 18:01:41 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import streamlit as st
 import pandas as pd
 import smtplib
 import re
+import time
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from streamlit_gsheets import GSheetsConnection
@@ -18,19 +19,10 @@ st.set_page_config(
 )
 
 ## Connect to Google Sheets
-conn = st.connection('gsheets', type=GSheetsConnection)
-df_blanks = conn.read(
-            worksheet='Blocos',
-            ttl=60
-)
-df_eps 	  = conn.read(
-            worksheet='EPS FQ',
-            ttl=60
-)
-df_dist = conn.read(
-            worksheet='Distribuidores',
-            ttl=60
-)
+conn = st.experimental_connection('gsheets', type=GSheetsConnection)
+df_blanks 	= conn.read(worksheet='Blocos', ttl=60)
+df_eps 	  	= conn.read(worksheet='EPS FQ', ttl=60)
+df_dist 	= conn.read(worksheet='Distribuidores', ttl=60)
 
 ## Title
 st.title(':ocean: Plataforma de Pedidos Teccel')
@@ -49,10 +41,26 @@ subtypes = ['TB','TecLight','TecGreen','Premium']
 ## Initialise session states
 if 'order_reviewed' not in st.session_state:
     st.session_state['order_reviewed'] = False
+if 'order_confirmed' not in st.session_state:
+    st.session_state['order_confirmed'] = False
+if 'human_verified' not in st.session_state:
+    st.session_state['human_verified'] = False
+if 'verification_attempted' not in st.session_state:
+    st.session_state['verification_attempted'] = False
 if 'order_text' not in st.session_state:
     st.session_state['order_text'] = ''
 if 'price' not in st.session_state:
     st.session_state['price'] = 0
+if 'last_submission_time' not in st.session_state:
+    st.session_state['last_submission_time'] = 0
+    
+    # Initialize math question in session state
+if 'num1' not in st.session_state:
+    st.session_state['num1'] = random.randint(0, 5)
+if 'num2' not in st.session_state:
+    st.session_state['num2'] = random.randint(0, 5)
+if 'correct_answer' not in st.session_state:
+    st.session_state['correct_answer'] = st.session_state['num1'] + st.session_state['num2']
 
 ## Function to validate email
 def is_valid_email(email):
@@ -124,8 +132,27 @@ def send_order(client_name, selected_distributor, order_text, recipient_emails):
     sender_password = st.secrets['email_password']
     subject = f'Pedido Teccel #{order_code}'
     send_email(sender_email, sender_password, recipient_emails, subject, new_order_text)
+    
+    # Reset session state
+    reset_session_state()
+    
+def reset_session_state():
+    
+    # Reset all relevant session state variables
+    st.session_state['order_reviewed'] = False
+    st.session_state['order_confirmed'] = False
+    st.session_state['human_verified'] = False
+    st.session_state['order_text'] = ''
+    st.session_state['price'] = 0
+    st.session_state['verification_attempted'] = False
+    
+    # Reset math question
+    st.session_state['num1'] = random.randint(0, 5)
+    st.session_state['num2'] = random.randint(0, 5)
+    st.session_state['correct_answer'] = st.session_state['num1'] + st.session_state['num2']
 
-
+    
+    
 # --- UI --- #
 # TAB: Enviar pedido
 with tabs[0]:
@@ -158,7 +185,7 @@ with tabs[0]:
         
         # Select items
         st.write('Digite a quantidade que deseja de cada item.')
-        df_order_editor = st.data_editor(df_order, width=500, height=500)
+        df_order_editor = st.experimental_data_editor(df_order, width=500, height=500)
         
         if st.button('Revisar Pedido'):
             
@@ -189,11 +216,9 @@ with tabs[0]:
                         product_amount += 1
             
             if product_amount > 0:
-                
                 st.session_state['order_text'] = order_text
         
             else:
-                
                 st.session_state['order_text'] = 'O usuário não escolheu nenhum produto.'
         
         # Order review
@@ -210,9 +235,50 @@ with tabs[0]:
             distributor_email = df_dist.loc[df_dist['Nome'] == selected_distributor, 'Email'].values[0]
             recipient_emails = [teccel_email, distributor_email, client_email]
             
-            if st.button('Enviar Pedido'):
+            if st.button('Confirmar Pedido'):
+                st.session_state['order_confirmed'] = True
+            
+            if st.session_state['order_confirmed']:
+                
                 if is_valid_email(client_email):
-                    send_order(client_name, selected_distributor, st.session_state['order_text'], recipient_emails)
+                    
+                    if not st.session_state['human_verified']:
+                        
+                        # Math question
+                        st.write('Por questões de segurança, precisamos verificar que você é um humano.')
+                        st.write(f'Pergunta de verificação: quanto é {st.session_state["num1"]} + {st.session_state["num2"]}?')
+                        user_answer = st.number_input('Sua resposta', value=0)
+                        
+                        num1 = random.randint(0,5)
+                        num2 = random.randint(0,5)
+                        correct_answer = num1 + num2
+                        
+                        # Verify answer
+                        if st.button('Verificar'):
+                            st.session_state['verification_attempted'] = True
+                            
+                            if user_answer == st.session_state['correct_answer']:
+                                st.session_state['human_verified'] = True
+                                st.write('Verificação bem sucedida. Pressione ENVIAR para finalizar.')
+                            else:
+                                st.error('Resposta incorreta. Tente novamente.')
+                    
+                    if st.session_state['human_verified']:
+                            
+                        # Rate limit
+                        current_time = time.time()
+                        rate_limit_period = 120 # 2 minutes
+                        time_since_last_submission = current_time - st.session_state['last_submission_time']
+                        
+                        if time_since_last_submission < rate_limit_period:
+                            st.warning('Por questões de segurança, um usuário só pode enviar um pedido a cada 2 minutos. Por favor, aguarde antes de tentar novamente.')
+                        else:
+                            
+                            # Send order
+                            if st.button('Enviar'):
+                                st.session_state['last_submission_time'] = current_time
+                                send_order(client_name, selected_distributor, st.session_state['order_text'], recipient_emails)
+                    
                 else:
                     st.warning('O email inserido é inválido.')
     
@@ -225,6 +291,8 @@ with tabs[0]:
             df_catalog[blank_type] = df_catalog[blank_type].astype(int)
             df_catalog[blank_type] = df_catalog[blank_type].apply(lambda x: f'R$ {x}')
         st.dataframe(df_catalog)
+        
+        
         
 with tabs[1]:
     
